d59555fb7664ce6e040dac003f9f9efe44edc4af
Shyamsundar S S <shyamsundar93424@gmail.com>
2026-08-13 12:03:11 +0530

    reverted

--- restaurant-service/config/jwt.ts
+++
@@ -1,7 +0,0 @@
-import Env from '@ioc:Adonis/Core/Env'
-
-export const jwtConfig = {
-  secret: Env.get('JWT_SECRET', 'super_secret_jwt_key_adonis_user_service'),
-}
-
-export default jwtConfig
--- restaurant-service/start/kernel.ts
+++
@@ -1,8 +0,0 @@
-import Server from '@ioc:Adonis/Core/Server'
-
-Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
-
-Server.middleware.registerNamed({
-  jwtAuth: () => import('App/Middleware/JwtAuth'),
-  role: () => import('App/Middleware/RoleMiddleware'),
-})
